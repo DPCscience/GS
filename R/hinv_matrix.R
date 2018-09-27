f9162c32c42afea8c10265d7296e8860f6f3dd03
dengfei2013 <dengfei_2013@163.com>
2018-09-27 14:25:58 +0800

    simple H matrix

--- R/hinv_matrix.R
+++ R/hinv_matrix.R
@@ -50,132 +50,30 @@
 
 
 hinv_matrix <- function(M012,ped_full,diagadd=0.001){
-
-  cat(rep("******",10),"\n")
   cat("G matrix diagonal add:",diagadd,"\n")
   library(MASS)
   library(sommer)
   library(nadiv)
 
-  Timex = proc.time() # begin
-  cat("Begin to build G matrix... \n") # begin
   G <- A.mat(M012-1)
   diag(G)=diag(G) + diagadd
-  Timex = as.matrix(proc.time() - Timex) #end
-  cat("\n", "G matrix takes time =", Timex[3]/60, " minutes \n\n\n") #end
-
-
-  Timex = proc.time() # begin
-  cat("Begin to build A matrix... \n") # begin
 
   A <- as.matrix(makeA(prepPed(ped_full)))
   iA <- solve(A)
   rownames(iA) <- colnames(iA) <- rownames(A)
+  idg=rownames(G)
 
-  Timex = as.matrix(proc.time() - Timex) #end
-  cat("\n", "A matrix takes time =", Timex[3]/60, " minutes \n\n\n") #end
-
-
-  genotyped=rownames(G)
-  diagG <- diag(G)
-  cat(rep("******",10),"\n")
-  options(digits = 2)
-  cat("Frequency - Diagonal of G\n","\tN:\t\t",length(diagG),"\n",
-      "\tMean:\t\t",mean(diagG),"\n",
-      "\tMin:\t\t",min(diagG),"\n",
-      "\tMax:\t\t",max(diagG),"\n",
-      "\tRange:\t\t",range(diagG),"\n")
-  options(digits = 7)
-
-  inpedigree=colnames(A)
-  nongenotyped=setdiff(inpedigree,genotyped)
-
-  cat("\n\nIn pedigree nongenotyped length: ",length(nongenotyped),"\n")
-
-  genotypednotinpedigree=setdiff(genotyped,inpedigree)
-  cat("genotyped not in pedigree",length(genotypednotinpedigree),"\n")
-
-  genotypedinpedigree=intersect(genotyped,inpedigree)
-  cat("genotyped in pedigree",length(genotypedinpedigree),"\n")
-
-  genotypedinpedigree <- as.character(genotypedinpedigree)
-
-  G=G[genotypedinpedigree,genotypedinpedigree]
-  genotyped=genotypedinpedigree
-
-
-  genotype <- as.character(genotyped)
-  nongenotyped <- as.character(nongenotyped)
-
-  A22 <- A[genotype,genotype]
-  diagA22 <- diag(A22)
-  offdiagA22 <- c(A22[upper.tri(A22)],A22[lower.tri(A22)])
-  options(digits = 2)
-  cat("\n\nStatistic of Rel Matrix A22\n","\t\t","N","\t","Mean","\t","Min","\t","Max","\t","Var","\n",
-      "Diagonal\t",length(diagA22),"\t",mean(diagA22),"\t",min(diagA22),"\t",max(diagA22),"\t",var(diagA22),
-      "\nOff-diagonal\t",length(offdiagA22),"\t",mean(offdiagA22),"\t",min(offdiagA22),"\t",max(offdiagA22),"\t",var(offdiagA22),"\n\n")
-  options(digits = 7)
-
-
-  diagG <- diag(G)
-  offdiagG <- c(G[upper.tri(G)],G[lower.tri(G)])
-  options(digits = 2)
-  cat("Statistic of Genomic Matrix\n","\t\t","N","\t","Mean","\t","Min","\t","Max","\t","Var","\n",
-      "Diagonal\t",length(diagG),"\t",mean(diagG),"\t",min(diagG),"\t",max(diagG),"\t",var(diagG),
-      "\nOff-diagonal\t",length(offdiagG),"\t",mean(offdiagG),"\t",min(offdiagG),"\t",max(offdiagG),"\t",var(offdiagG),"\n\n")
-  options(digits = 7)
-
-  cat("Correlation of Genomic Inbreeding and Pedigree InbreedingA22\n",
-      "\tCorrelation:",cor(diagA22,diagG),"\n\n\n")
-  cat("Correlation Off-diagonal G and A22\n",
-      "\tCorrelation:",cor(offdiagA22,offdiagG),"\n\n\n")
-
+  id=colnames(A)
+  ida=setdiff(id,idg)
+  A22 <- A[idg,idg]
   iA22 <- solve(A22)
-
-  cat(rep("******",10),"\n")
-  cat("G and A22 Inv matrix\n")
-  cat(rep("******",10),"\n")
-
-  meanoffdiagG=mean(offdiagG)
-  meandiagG=mean(diagG)
-  meanoffdiagA22=mean(offdiagA22)
-  meandiagA22=mean(diagA22)
-  cat("Means_off_diag\t","Means_diag:\n","G\t",meanoffdiagG,"\t",meandiagG,
-      "\nA22\t",meanoffdiagA22,"\t",meandiagA22,"\n")
-  beta=(meandiagA22 - meanoffdiagA22)/(meandiagG - meanoffdiagG)
-  alpha=meandiagA22-meandiagG*beta
-  cat("No-Adjust G, the value of alpha and beta is:",alpha,beta,"\n\n\n")
-  # G=alpha+beta*G #
-  # G = a*G + b*A22
-  Timex = proc.time() # begin
-  cat("Begin to inverse G matrix... \n\n") # begin
-
   iG <- solve(G)
-  rownames(iG) = colnames(iG) = genotyped
 
-  Timex = as.matrix(proc.time() - Timex) #end
-  cat("\n", "Inverse G matrix takes time =", Timex[3]/60, " minutes \n\n\n") #end
-
-  iG1 <- iG[genotype,genotype]
-  rownames(iA22) = colnames(iA22) <- row.names(A22)
-  # x22 <- tau*iG1 - omega*iA22
-  x22 <- iG1 - iA22
-  diagx22 <- diag(x22)
-  offdiagx22 <- c(x22[upper.tri(x22)],x22[lower.tri(x22)])
-  options(digits = 2)
-  cat("Statistic of iG - iA22 Matrix\n","\t\t","N","\t","Mean","\t","Min","\t","Max","\t","Var","\n",
-      "Diagonal\t",length(diagx22),"\t",mean(diagx22),"\t",min(diagx22),"\t",max(diagx22),"\t",var(diagx22),
-      "\nOff-diagonal\t",length(offdiagx22),"\t",mean(offdiagx22),"\t",min(offdiagx22),"\t",max(offdiagx22),"\t",var(offdiagx22),"\n\n")
-  options(digits = 7)
-
-
-  iH11 <- iA[nongenotyped,nongenotyped]
-  iH21 <- iA[genotype,nongenotyped]
+  x22 <- iG - iA22
+  iH11 <- iA[ida,ida]
+  iH21 <- iA[idg,ida]
   iH12 <- t(iH21)
-  iH22 <-  iA[genotype,genotype] + x22
+  iH22 <-  iA[idg,idg] + x22
   Hinv <- cbind(rbind(iH11,iH21),rbind(iH12,iH22))
-
-  Time = as.matrix(proc.time() - Time) #end
-  cat("\n", "hinv_matrix completed! total time =", Time[3]/60, " minutes \n\n\n") #end
   return(Hinv)
 }
